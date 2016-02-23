88f9dabb511495d22d60700964908cea601a4aae
Luis-bcruz <luis.labc95@gmail.com>
2016-02-23 17:20:49 +0000

    Alteracao ao Dragao

--- Dragao.java
+++ Dragao.java
@@ -1,3 +1,6 @@
+package maze.logic;
+import java.util.Random;
+
 
 public class Dragao extends Peca {
 	
@@ -10,6 +13,25 @@ public class Dragao extends Peca {
 	}
 	public int getY(){
 		return y;
+	}
+	public char dragaomove(){
+		Random randomdragao = new Random();
+		int numero = randomdragao.nextInt(100)+1;
+		
+		if(numero < 20)
+			return 'w';
+		else if(numero > 20 && numero < 40)
+			return 'a';
+		else if(numero > 40 && numero < 60)
+			return 'd';
+		else if(numero > 60 && numero < 80)
+			return 's';
+		else
+			return 'm';
+	}
+	public void Move(int x,int y){
+		this.x=x;
+		this.y=y;
 	}
 
 }
